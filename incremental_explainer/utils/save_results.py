b00d1e23ae636d86ab0d499ab23970b7c49a5f2d
Santiago Calderón <santiago.calderon@quorumsoftware.com>
2024-05-26 18:46:00 +0100

    Add save results script

---
+++ incremental_explainer/utils/save_results.py
@@ -0,0 +1,25 @@
+import pickle
+import os
+
+PICKLE_FILE = 'results.pkl'
+def save_results(car_number: int, start_frame: int, end_frame:int, insertion_move: list[int], insertion_exp: list[int]):
+    row = {
+        'car_number': car_number,
+        'start_frame': start_frame,
+        'frames_number': end_frame,
+        'insertion_move': insertion_move,
+        'insertion_exp': insertion_exp,
+           }
+    
+    
+    if os.path.exists(PICKLE_FILE):
+        with open(PICKLE_FILE, 'rb') as file:
+            data = pickle.load(file)
+            data.append(row)
+        with open(PICKLE_FILE, 'wb') as file:
+            pickle.dump(data, file)
+    else:
+        # Create the pickle file and write default data to it
+        with open(PICKLE_FILE, 'wb') as file:
+            pickle.dump([row], file)
+            data = PICKLE_FILE
