8ff690521d9e6f2dadf81f1f1f8ae23f91f6757d
Santiago Calderón <santiago.calderon@quorumsoftware.com>
2024-05-26 19:35:41 +0100

    Create main.py file

---
+++ main.py
@@ -0,0 +1,33 @@
+import argparse
+from incremental_explainer.utils.video import create_video, save_video
+import matplotlib.pyplot as plt
+from incremental_explainer.tracking import track_saliency_maps
+from incremental_explainer.utils.save_results import save_results
+from ultralytics import YOLO
+import cv2
+
+def track_saliency_maps_with_video(frame_number, car_set_object, box_index):
+    print(f"Frame number: {frame_number}, Car number: {car_set_object}, Explanation index: {box_index}")
+    frames, auc_results, aux_results_2 = track_saliency_maps(frame_number=frame_number, car_number=car_set_object, box_index_first_frame=box_index)
+    print(f"Number of frames: {len(frames)}")
+    save_results(car_set_object, frame_number, len(frames), auc_results, aux_results_2)
+
+def main(car_number, frame_number):
+    model = YOLO("yolov8n.pt")
+    image_location = f"datasets/car/car-{car_number}/img/{str(frame_number).zfill(8)}.jpg"
+    img = cv2.imread(image_location)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = model(img, verbose=False)
+    number_objects = len(results[0].boxes.cls)
+    print(f"Number of objects: {number_objects}")
+    for i in range(number_objects):
+        track_saliency_maps_with_video(frame_number, car_number, i)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('car_number', type=int, help='The number of the car to process')
+    parser.add_argument('frame_number', type=int, help='The frame number to process')
+
+    args = parser.parse_args()
+
+    main(args.car_number, args.frame_number)
