2826e11f523e648e5a6ef4ac50c7270f2d6b57f9
SantiagoCalderon1999 <santiagocalderon1999@gmail.com>
2024-05-27 14:02:21 +0100

    Iterate every 50 frames

--- multiple_batch.sh
+++ multiple_batch.sh
@@ -1,7 +1,9 @@
 # CPUs limit is roughly 380 (Estimation)
 for i in {1..20}
 do
-    for j in {1,100,200,300,400,500,600,700,800,900,1000}
+    for (( j=1; j<=2500; j+=50 ))
+    do
         sbatch batch.sh $i $j
     done
 done
+
