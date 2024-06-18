9cb202f8c6985b057639f325ccb75cebb6103c37
Santiago Calderón <santiago.calderon@quorumsoftware.com>
2024-06-18 19:21:50 +0100

    Fix insertion metric

--- incremental_explainer/metrics/insertion.py
+++ incremental_explainer/metrics/insertion.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import metrics
 from numpy import trapz
+from incremental_explainer.utils.common import calculate_intersection_over_union
 
-def compute_insertion(model, saliency_map, image, class_index, divisions=100):
+def compute_insertion_old(model, saliency_map, image, class_index, divisions=100):
 
         masks = np.empty([saliency_map.shape[0], saliency_map.shape[1], 3])
         conf_insertion_list = []
@@ -37,7 +38,7 @@ def compute_insertion(model, saliency_map, image, class_index, divisions=100):
     
         return trapz(conf_insertion_list, divisions_list_in)
     
-def compute_insertion_array(model, saliency_map, image, class_index, divisions=100):
+def compute_insertion(model, saliency_map, image, class_index, bounding_box, divisions=100, verbose = False):
         import matplotlib as mpl
         mpl.rcParams["savefig.pad_inches"] = 0
         masks = np.empty([saliency_map.shape[0], saliency_map.shape[1], 3])
@@ -49,29 +50,34 @@ def compute_insertion_array(model, saliency_map, image, class_index, divisions=1
         maximum = np.max(saliency_map) + np.abs(
             (np.min(saliency_map) - np.max(saliency_map)) * 0.1
         )
+        im_size = saliency_map.shape[0] * saliency_map.shape[1] * 3
         for sub_index in range(0, divisions):
             masks[:, :, :] = False
             threshold = maximum + (sub_index / divisions) * (minimum - maximum)
             pixels = np.where(saliency_map >= threshold)
             masks[pixels[0], pixels[1], :] = True
+            div = len(pixels[0]) / (im_size)
             divisions_list_in.append(
-                len(pixels[0]) / (saliency_map.shape[0] * saliency_map.shape[1])
+                div
             )
             min_expl = np.where(masks, image, 0)
             detection = model(min_expl, classes=class_index, verbose=False)
-            conf = (
-                detection[0].boxes.conf[0]
-                if (len(detection[0].boxes.conf) > 0)
-                else detection[0].boxes.conf
-            )
-            print(sub_index)
-            # plt.axis('off')
-            # plt.imshow(min_expl)
-            # plt.show()
-            conf_insertion_list.append(conf)
-        conf_insertion_list = [
-            np.sum(conf.numpy()) if conf.numpy().size > 0 else 0
-            for conf in conf_insertion_list
-        ]
+            
+            arrays = list(zip(np.array(detection[0].boxes.cls), np.array(detection[0].boxes.conf), np.array(detection[0].boxes.xyxy)))
+
+            arrays = [el for el in arrays if el[0] == class_index]
+
+            if len(arrays) > 0:
+                max_confidence = max([el[1]* calculate_intersection_over_union(bounding_box, el[2]) for el in arrays])
+            else:
+                max_confidence = 0
+
+            conf_insertion_list.append(max_confidence)
+
+        auc = trapz(conf_insertion_list, divisions_list_in)
+        
+        if verbose:
+            plt.plot(divisions_list_in, conf_insertion_list)
+            plt.title(f'Insertion curve - AUC = {auc}')
     
-        return conf_insertion_list, divisions_list_in, trapz(conf_insertion_list, divisions_list_in)
+        return auc
