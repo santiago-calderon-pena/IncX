5cc9b5a772fa8afb5e751dec4d600c9de10cdf19
Santiago Calderón <santiago.calderon@quorumsoftware.com>
2024-06-19 16:53:48 +0100

    Refactor drise saliency maps

--- incremental_explainer/utils/saliency_maps.py
+++ incremental_explainer/utils/saliency_maps.py
@@ -2,12 +2,12 @@ from vision_explanation_methods import DRISE_runner as dr
 from helpers.pytorch_yolov8_wrapper import PytorchYoloV8Wrapper
 from ultralytics import YOLO
 
-def compute_saliency_maps(results, image_location, model):
+def compute_saliency_maps(results, image_location, model, nummasks=50):
     # print(image_location)
     # print(results)
     number = 0
     while (len(results[0].boxes.cls) != number): # Check assumption that it sometimes cannot find the same number of objects
-        results_drise = dr.get_drise_saliency_map(nummasks=5000, imagelocation=image_location, model= PytorchYoloV8Wrapper(model), savename="anything", numclasses=80, max_figures=2, maskres=(16,16))
+        results_drise = dr.get_drise_saliency_map(nummasks=nummasks, imagelocation=image_location, model= PytorchYoloV8Wrapper(model), savename="anything", numclasses=80, max_figures=2, maskres=(16,16))
         number = len(results_drise)
     # print("Number of objects found in expl: ", number)
     # print("Number of objects found in bounding box: ", len(results[0].boxes.cls))
