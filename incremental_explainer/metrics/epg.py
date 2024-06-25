c942bd27050e75d96bd6d3f7eab374e87f9f29d5
Santiago Calderón <santiago.calderon@quorumsoftware.com>
2024-06-25 16:13:15 +0100

    Fix epg order of saliency map indices

--- incremental_explainer/metrics/epg.py
+++ incremental_explainer/metrics/epg.py
@@ -6,6 +6,6 @@ def compute_energy_based_pointing_game(saliency_map, bounding_box):
 
     x_0, y_0, x_1, y_1 = bounding_box
     mask = np.zeros_like(saliency_map)
-    mask[floor(x_0):ceil(x_1), floor(y_0):ceil(y_1)] = 1
+    mask[floor(y_0):ceil(y_1), floor(x_0):ceil(x_1)] = 1
     saliency_map_masked = saliency_map * mask
     return sum([sum(row) for row in saliency_map_masked]) / sum([sum(row) for row in saliency_map])
