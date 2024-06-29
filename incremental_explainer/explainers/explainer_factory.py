444ed78a16b91e1a223886e944a8392e63fa5b9e
Santiago Calderón <santiagocalderon1999@gmail.com>
2024-06-29 14:07:55 +0100

    Improve explainer factory accomodating new explainer parameters

--- incremental_explainer/explainers/explainer_factory.py
+++ incremental_explainer/explainers/explainer_factory.py
@@ -1,15 +1,15 @@
 from incremental_explainer.explainers.explainer_enum import ExplainerEnum
 from incremental_explainer.explainers.d_rise import DRise
 from incremental_explainer.explainers.base_explainer import BaseExplainer
-
+from incremental_explainer.models.base_model import BaseModel
 
 class ExplainerFactory:
     
-    def __init__(self, results) -> None:
-        self._results = results
+    def __init__(self,  model: BaseModel) -> None:
+        self._model = model
     
     def get_explainer(self, explainer: ExplainerEnum) -> BaseExplainer:
         if explainer == ExplainerEnum.D_RISE:
-            return DRise(self._results)
+            return DRise(self._model)
 
         raise ValueError(f"Unsupported explainer identifier: {explainer}")
