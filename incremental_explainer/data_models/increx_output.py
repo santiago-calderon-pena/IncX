74a457720135679fd4190c7f5ba4dc7a00844bce
Santiago Calderón <santiagocalderon1999@gmail.com>
2024-06-30 17:29:03 +0100

    Create IncRexOutput

---
+++ incremental_explainer/data_models/increx_output.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+import numpy as np
+from typing import List
+from pydantic import BaseModel, Field
+
+class IncRexOutput(BaseModel):
+    saliency_map: np.ndarray
+    bounding_box: List[int]
+    sufficient_explanation: np.ndarray
+
+    class Config:
+        arbitrary_types_allowed = True
