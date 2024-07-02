979f446efe9367ad9f147af120468c89db5240fe
Santiago Calderón <santiagocalderon1999@gmail.com>
2024-07-02 14:21:36 +0100

    Implement binary search

--- incremental_explainer/utils/explanations.py
+++ incremental_explainer/utils/explanations.py
@@ -5,23 +5,36 @@ from incremental_explainer.utils.common import calculate_intersection_over_union
 import torchvision.transforms as transforms
     
 
-def compute_initial_sufficient_explanation(model: BaseModel, saliency_map, image, class_index, bounding_box, divisions=100):
+def compute_initial_sufficient_explanation(model: BaseModel, saliency_map, image, class_index, bounding_box, divisions=100, minimum=None, maximum=None):
     masks = np.zeros((saliency_map.shape[0], saliency_map.shape[1], 3), dtype=bool)
 
-    minimum = np.min(saliency_map) - np.abs(
-        (np.min(saliency_map) - np.max(saliency_map)) * 0.2
-    )
-    maximum = np.max(saliency_map) + np.abs(
-        (np.min(saliency_map) - np.max(saliency_map)) * 0.1
-    )
+    if minimum is None:
+        minimum = np.min(saliency_map)
+        
+    if maximum is None:
+        maximum = np.max(saliency_map)
 
     transform = transforms.Compose([
         transforms.ToTensor()
     ])
+    thresholds = np.linspace(start=minimum, stop=maximum, num=divisions).tolist()
+    left = 0
+    right = len(thresholds) - 1
     
-    for sub_index in range(divisions):
+    if minimum is None and maximum is None:
+        final_suf_expl = np.zeros_like(image)
+        final_threshold = minimum
+    else:
+        final_threshold = (minimum + maximum) / 2
         masks.fill(False)
-        threshold = maximum + (sub_index / divisions) * (minimum - maximum)
+        pixels = np.where(saliency_map >= final_threshold)
+        masks[pixels[0], pixels[1], :] = True
+        final_suf_expl = np.where(masks, image, 0)
+        
+    while left < right:
+        t_index = (left + right) // 2
+        threshold = thresholds[t_index]
+        masks.fill(False)
         pixels = np.where(saliency_map >= threshold)
         masks[pixels[0], pixels[1], :] = True
 
@@ -43,10 +56,14 @@ def compute_initial_sufficient_explanation(model: BaseModel, saliency_map, image
         else:
             max_confidence = 0
 
-        if max_confidence > 0.3:
-            return suf_expl, threshold
-
-    return np.zeros_like(image), threshold
+        if max_confidence > 0:
+            left = t_index + 1
+            final_suf_expl = suf_expl
+            final_threshold = threshold
+        else:
+            right = t_index - 1
+    
+    return final_suf_expl, final_threshold
     
 def compute_subsequent_sufficient_explanation(saliency_map, image, threshold):
     masks = np.empty([saliency_map.shape[0], saliency_map.shape[1], 3])
