0bb6cd385744e34c109f80cd7cbe412340a3f9fe
Santiago Calderón <santiagocalderon1999@gmail.com>
2024-08-10 19:13:10 +0100

    Remove outdated shell scripts

--- batch_experiment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --job-name=incremental_baseline
-python experiment.py
--- batch_incx_gpu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --job-name=incx_gpu
-#SBATCH --gres=gpu
-#SBATCH -p biomed_a100_gpu 
-python experiments/incx/get_saliency_maps.py
